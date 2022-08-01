fb5f8c23de2e75e9e8d9ef613f5d5956353423a0
Jesus Vasquez M <jesusvasquezm098@gmail.com>
2022-08-01 20:22:47 -0300

    Adicionar pessoas na base de dados

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -1,19 +1,33 @@
 package application;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 import dominio.Pessoa;
 
 public class Program {
 
 	public static void main(String[] args) {
 
+		//Atribuir ID nulo pois o proprio banco de dados vai atribuir um ID para p1,p2,p3
+		Pessoa p1 = new Pessoa(null, "Carlos da Silda", "carlos@gmail.com");
+		Pessoa p2 = new Pessoa(null, "Joaquim Torres", "joaquim@gmail.com");
+		Pessoa p3 = new Pessoa(null, "Ana Maria", "maria@gmail.com");
+		
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
+		EntityManager em = emf.createEntityManager();
+		
+		em.getTransaction().begin(); // Inicia um trasacção om o banco de dados
+		em.persist(p1);
+		em.persist(p2);
+		em.persist(p3);
+		em.getTransaction().commit(); // Confirma as alterações feitas
+		System.out.println("Pronto");
+		
+		em.close();
+		emf.close();
 
-		Pessoa p1 = new Pessoa(1, "Carlos da Silda", "carlos@gmail.com");
-		Pessoa p2 = new Pessoa(2, "Joaquim Torres", "joaquim@gmail.com");
-		Pessoa p3 = new Pessoa(3, "Ana Maria", "maria@gmail.com");
-
-		System.out.println(p1);
-		System.out.println(p2);
-		System.out.println(p3);
 	}
 
 }
